c0e1fa2e96e594b5c71130e1c97a69e6e9e08bc0
melek99klss <kelesmelek739@gmail.com>
2026-05-21 18:38:32 +0300

    Added VIEWs for restaurant menu and donation system

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -47,6 +47,47 @@ INNER JOIN Customer c ON o.CustomerID = c.CustomerID
 INNER JOIN Restaurant r ON o.RestaurantID = r.RestaurantID
 INNER JOIN OrderDetails od ON o.OrderID = od.OrderID
 INNER JOIN Product p ON od.ProductID = p.ProductID;
+-- GROUP BY ANALÝZ SORGUSU
+
+SELECT 
+    r.Name AS RestaurantName,
+    COUNT(o.OrderID) AS TotalOrders,
+    SUM(o.TotalAmount) AS TotalRevenue,
+    AVG(o.TotalAmount) AS AvgOrderValue
+FROM Restaurant r
+INNER JOIN Orders o ON r.RestaurantID = o.RestaurantID
+GROUP BY r.Name;
 
 
 
+-- SUBQUERY SORGUSU
+
+SELECT 
+    c.CustomerID,
+    c.FullName,
+    c.Email
+FROM Customer c
+WHERE c.CustomerID NOT IN (
+    SELECT d.CustomerID
+    FROM Donation d
+);
+CREATE VIEW vw_AktifRestoranMenuleri AS
+SELECT 
+    r.RestaurantID,
+    r.Name AS RestaurantName,
+    p.ProductID,
+    p.Name AS ProductName,
+    p.Price
+FROM Restaurant r
+INNER JOIN Product p ON r.RestaurantID = p.RestaurantID
+WHERE r.IsActive = 1 AND p.IsActive = 1;
+CREATE VIEW vw_AskidaYemekHavuzu AS
+SELECT 
+    c.CustomerID,
+    c.FullName,
+    d.DonationID,
+    d.Amount,
+    d.IsAnonymous,
+    d.DonationDate
+FROM Donation d
+LEFT JOIN Customer c ON d.CustomerID = c.CustomerID;
